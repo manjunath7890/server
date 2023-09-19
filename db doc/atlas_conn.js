639b5a74ece9c45c1752ca741a97e21e6fba3e7c
Manjunath <flyingfortress289@gmail.com>
2023-09-19 12:31:44 +0530

    b commit

--- db doc/atlas_conn.js
+++ db doc/atlas_conn.js
@@ -1,10 +1,10 @@
 // add atlas req along with url
 
 const mongoose = require('mongoose');
-const dbb = process.env.db;
+const db = process.env.db;
 // const dbb = "mongodb+srv://flyingfortress289:flyingfortress289@cluster0.zlhd1zd.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connect(dbb, {
+mongoose.connect(db, {
     useNewUrlParser: true,
     // useCreateIndex:true,
     useUnifiedTopology: true
