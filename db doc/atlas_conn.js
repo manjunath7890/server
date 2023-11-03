05f60a030028c1fd0ac9ccdea15aafb799a95eef
manjunath7890 <95606411+manjunath7890@users.noreply.github.com>
2023-11-03 12:25:37 +0530

    Update atlas_conn.js

--- db doc/atlas_conn.js
+++ db doc/atlas_conn.js
@@ -1,7 +1,7 @@
 // add atlas req along with url
 
 const mongoose = require('mongoose');
-const dbb = process.env.db;
+const dbb =  'mongodb+srv://flyingfortress289:flyingfortress289@cluster0.zlhd1zd.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(dbb, {
     useNewUrlParser: true,
